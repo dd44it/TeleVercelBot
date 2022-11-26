7394dabc021c3824028e017063c15188a4ed7333
dd44it <113500552+dd44it@users.noreply.github.com>
2022-11-26 18:43:11 +0200

    added command start handler

--- bot.mjs
+++ bot.mjs
@@ -2,6 +2,8 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
+bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/500') )
+
 bot.on('text', msg => msg.reply.text(msg.text))
 
 export default bot
